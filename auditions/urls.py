f663c0fac9aa9fd20bfe5fe08a4d91cdc56d917b
Yacob Yonas <yyonas@princeton.edu>
2014-09-12 17:32:15 -0400

    add comma

--- auditions/urls.py
+++ auditions/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    
+
     url(r'^group_admin/?$', 'auditions.views.group_admin'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^add_remove_callbacks$', 'auditions.views.add_remove_callbackee'),
@@ -16,6 +16,6 @@ urlpatterns = patterns('',
     url(r'^site_admin', 'auditions.views.site_admin'),
     url(r'^send_callbackee_emails', 'auditions.views.send_callbackee_emails'),
     url(r'^group_results', 'auditions.views.view_groups_results'),
-    url(r'^logout', 'auditions.views.logout')
+    url(r'^logout', 'auditions.views.logout'),
     url(r'^', 'auditions.views.root'),
 )
